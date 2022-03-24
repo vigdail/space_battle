f0fa15347fcb4b72876173bc01f2959d3ee9dc1e
kmb2 <kmb2@tpu.ru>
2022-03-24 13:34:35 +0600

    Fixed window size

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ use game_plugin::GamePlugin;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.insert_resource(WindowDescriptor {
+        width: 800.0,
+        height: 600.0,
+        resizable: false,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins);
     #[cfg(feature = "debug")]
     app.add_plugin(WorldInspectorPlugin::default());
     app.add_plugin(GamePlugin).run();
